4ad556d0a8acce3e02f36544c5c55aed329a899f
dianaradueva <32335872+dianaradueva@users.noreply.github.com>
2018-11-06 16:01:10 +0100

    login

--- Hybride/Server/registartie.php
+++ Hybride/Server/registartie.php
@@ -1,11 +1,12 @@
 <?php
 $con = mysqli_connect("localhost", "alexandraklochkova", "alexandraklochkovaMI3", "events_db");
-$name = $_REQUEST["name"];
-$username = $_REQUEST["username"];
-$password = $_REQUEST["password"];
-$age = $_REQUEST["age"];
-     $statement = mysqli_prepare($con, "INSERT INTO accounts (name, username,password, age) VALUES (?, ?, ?, ?)") or die(mysqli_error($con));
-mysqli_stmt_bind_param($statement, "sssi", $name, $username, $password, $age);
+$naam = $_REQUEST["naam"];
+$voornaam = $_REQUEST["voornaam"];
+$gebruikersnaam = $_REQUEST["gebruikersnaam"];
+$wachtwoord = $_REQUEST["wachtwoord"];
+$email = $_REQUEST["email"];
+     $statement = mysqli_prepare($con, "INSERT INTO account (naam, voornaam,gebruikersnaam, wachtwoord, email) VALUES (?, ?, ?, ?, ?)") or die(mysqli_error($con));
+mysqli_stmt_bind_param($statement, "sssss", $naam, $voornaam, $wachtwoord, $email);
 mysqli_stmt_execute($statement);
 $response = "true";
 $req_dump = print_r($_REQUEST, TRUE);
