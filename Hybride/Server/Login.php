765f974ccdcd335145b7e28ade33355ca0ed9ead
maximegheraille <32335849+maximegheraille@users.noreply.github.com>
2018-11-15 09:06:43 +0100

    login proberen fixen

--- Hybride/Server/Login.php
+++ Hybride/Server/Login.php
@@ -10,11 +10,16 @@ $con = mysqli_connect("localhost", "dianaradueva", "dianaraduevaMI3", "events");
         $count = mysqli_num_rows($result);
         if ($count == 1){
             echo "true";
+            
         }else{
             echo "false";
         }
-        
+        echo'test';
     }
-    echo 'erte';
+    $req_dump = print_r($_REQUEST, TRUE);
+    $fp = fopen('request.log', 'a');
+    fwrite($fp, $req_dump);
+    fclose($fp);
+    
     
 ?>
--- js/Connectie.js
+++ js/Connectie.js
@@ -3,16 +3,28 @@ function logincheck() {
     var ww = $("#wwoord").val();
     var link = "www.gheraille.be/events/login.php?gebruikersnaam=" + gn + "&" + "wachtwoord=" + ww;
     $.ajax({
-        type: "post",
+        type: 'POST',
         url: link,
+        data: {
+            gebruikersnaam: gn,
+            wachtwoord: ww
+        },
         success: function (response) {
-            var obj = JSON.parse(response);
-            console.log(obj);
-            console.log("oker");
+            //            var obj = JSON.parse(response);
+            //          console.log(obj);
+            //        console.log(response);
+            if (response == "true") {
+                window.location.href = "index.html";
+                console.log("test");
+            } else {
+                Console.log("false");
+            }
         }
+
     });
     //console.log("");
-    //console.log(gn);
-    //console.log(ww);
+    // console.log(gn);
+    // console.log(ww);
+    console.log("test");
     console.log(link);
 }
