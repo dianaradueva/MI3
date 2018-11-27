5e637f50630743ec463f15c6e7cfb907bd434f14
maximegheraille <32335849+maximegheraille@users.noreply.github.com>
2018-11-27 08:30:54 +0100

    icoon omwisselen

--- js/collapsible.js
+++ js/collapsible.js
@@ -9,19 +9,79 @@
 
   });
 
-  function changeicon() {
+  function changeicon1() {
       var text = document.getElementById("expand").textContent;
-      var text2 = document.getElementById("expand2").textContent;
-      var text3 = document.getElementById("expand3").textContent;
-      var text4 = document.getElementById("expand4").textContent;
-      var text5 = document.getElementById("expand5").textContent;
-      var text6 = document.getElementById("expand6").textContent;
-      var text7 = document.getElementById("expand7").textContent;
       var less = "expand_less";
       var more = "expand_more";
       if (text == more) {
           $("#expand").text("expand_less");
       } else if (text == less) {
           $("#expand").text("expand_more");
+      }
+  };
+
+  function changeicon2() {
+      var text2 = document.getElementById("expand2").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text2 == more) {
+          $("#expand2").text("expand_less");
+      } else if (text2 == less) {
+          $("#expand2").text("expand_more");
+      }
+  };
+
+  function changeicon3() {
+      var text3 = document.getElementById("expand3").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text3 == more) {
+          $("#expand3").text("expand_less");
+      } else if (text3 == less) {
+          $("#expand3").text("expand_more");
+      }
+  };
+
+  function changeicon4() {
+      var text4 = document.getElementById("expand4").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text4 == more) {
+          $("#expand4").text("expand_less");
+      } else if (text4 == less) {
+          $("#expand4").text("expand_more");
+      }
+  };
+
+  function changeicon5() {
+      var text5 = document.getElementById("expand5").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text5 == more) {
+          $("#expand5").text("expand_less");
+      } else if (text5 == less) {
+          $("#expand5").text("expand_more");
+      }
+  };
+
+  function changeicon6() {
+      var text6 = document.getElementById("expand6").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text6 == more) {
+          $("#expand6").text("expand_less");
+      } else if (text6 == less) {
+          $("#expand6").text("expand_more");
+      }
+  };
+
+  function changeicon7() {
+      var text7 = document.getElementById("expand7").textContent;
+      var less = "expand_less";
+      var more = "expand_more";
+      if (text7 == more) {
+          $("#expand7").text("expand_less");
+      } else if (text7 == less) {
+          $("#expand7").text("expand_more");
       }
   };
