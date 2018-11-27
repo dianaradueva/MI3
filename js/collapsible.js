9322500a4dce3a6206cd52bede0e3636dd03aeff
maximegheraille <32335849+maximegheraille@users.noreply.github.com>
2018-11-27 08:43:08 +0100

    icoontjes functie aanpassen

--- js/collapsible.js
+++ js/collapsible.js
@@ -9,19 +9,47 @@
 
   });
 
+  function test() {
+      var text = document.getElementById("expand").textContent;
+      var text2 = document.getElementById("expand").textContent;
+      var text3 = document.getElementById("expand3").textContent;
+      var text4 = document.getElementById("expand4").textContent;
+      var text5 = document.getElementById("expand5").textContent;
+      var text6 = document.getElementById("expand6").textContent;
+      var text7 = document.getElementById("expand7").textContent;
+
+      if (text == more || text2 == more || text3 == more || text4 == more || text5 == more || text6 == more || text7 == more) {
+
+      }
+  }
+
   function changeicon1() {
       var text = document.getElementById("expand").textContent;
       var less = "expand_less";
       var more = "expand_more";
       if (text == more) {
           $("#expand").text("expand_less");
+          $("#expand2").text("expand_more");
+          $("#expand3").text("expand_more");
+          $("#expand4").text("expand_more");
+          $("#expand5").text("expand_more");
+          $("#expand6").text("expand_more");
+          $("#expand7").text("expand_more");
       } else if (text == less) {
           $("#expand").text("expand_more");
+          $("#expand2").text("expand_less");
+          $("#expand3").text("expand_less");
+          $("#expand4").text("expand_less");
+          $("#expand5").text("expand_less");
+          $("#expand6").text("expand_less");
+          $("#expand7").text("expand_less");
+
+
       }
   };
 
   function changeicon2() {
-      var text2 = document.getElementById("expand2").textContent;
+      var text2 = document.getElementById("expand").textContent;
       var less = "expand_less";
       var more = "expand_more";
       if (text2 == more) {
